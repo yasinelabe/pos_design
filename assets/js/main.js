f6e28289b0dd8aa43ed2bdaa920e4dc55cf53521
yasin <yasinelabe@gmail.com>
2022-04-17 02:39:17 +0300

    added history modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,7 @@ document.querySelector('.modal_shadow').addEventListener('click', function (e) {
     // add hidden attribute to all modals
     document.querySelector('.modal_shadow').setAttribute('hidden', '');
     document.querySelector('.calculator_modal').setAttribute('hidden', '');
+    document.querySelector('.history_modal').setAttribute('hidden', '');
 });
 
 // show modal when calculator icon is clicked use little animation
@@ -12,5 +13,14 @@ document.querySelector('.calculator').addEventListener('click', function (e) {
     document.querySelector('.modal_shadow').removeAttribute('hidden');
     document.querySelector('.calculator_modal').removeAttribute('hidden');
     document.querySelector('.calculator_modal').classList.add('animate__fadeIn','animate__fadeOut');
+});
+
+
+// show modal when history icon is clicked use little animation
+document.querySelector('.history').addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector('.modal_shadow').removeAttribute('hidden');
+    document.querySelector('.history_modal').removeAttribute('hidden');
+    document.querySelector('.history_modal').classList.add('animate__fadeIn','animate__fadeOut');
 });
 
